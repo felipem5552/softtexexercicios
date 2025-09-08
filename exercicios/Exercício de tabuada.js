494a663eb13dbe310bacc497597afe8ba800f5f3
felipem5552 <felipemendes5552@gmail.com>
2025-09-08 20:45:36 -0300

    Delete exercicios/Exercício de tabuada.js

--- exercicios/Exercício de tabuada.js
+++
@@ -1,7 +0,0 @@
-//Escreva um programa que calcule e mostre a tabuada (até 10x) de um número qualquer, digitado pelo usuário.
-let numero = parseInt(prompt("Digite um número para ver a tabuada: "));
-
-for(let i = 1; i <= 10; i ++){
-let resultado = numero * i;
-console.log(numero + " x " + i + " = " + resultado);
-}
