6f8e0f2284614c44080eccc1a5f7302c985c9b2c
felipem5552 <felipemendes5552@gmail.com>
2025-09-10 09:40:00 -0300

    Update and rename resolucao.js to exercicios/Exercício Var 1.1.js

---
+++ exercicios/Exercício Var 1.1.js
@@ -0,0 +1,10 @@
+/*Exercício 1.1
+Faça um programa que exiba 3 números na tela.
+
+Dica - exibindo (ou "imprimindo") números na tela:*/
+var a = [22, 23, 24];
+
+for (var i = 0; i < a.length; i++) {
+  console.log(a[i]);
+}
+
