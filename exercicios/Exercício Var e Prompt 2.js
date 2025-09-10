75d04f6bc084bffd4acc61d03bd01bdbdf0fc786
felipem5552 <felipemendes5552@gmail.com>
2025-09-10 09:40:07 -0300

    Create Exercício Var e Prompt 2.js

---
+++ exercicios/Exercício Var e Prompt 2.js
@@ -0,0 +1,17 @@
+//Faça um programa que leia 3 números e exiba-os na tela.
+
+
+
+let num0 = parseInt(prompt("Informe o numero 1:"))
+let num1 = parseInt(prompt("Informe o numero 1:"))
+let num2 = parseInt(prompt("Informe o numero 1:"))
+
+console.log("Os numeros informados foram:" +num0+","+num1+","+num2)
+
+var v1,v2,v3;
+v1 =  parseInt(prompt("Informe o primeiro numero: "))
+console.log(v1)
+v2 =  parseInt(prompt("Informe o segundo numero : "))
+console.log(v2)
+v3 =  parseInt(prompt("Informe o terceiro numero: "))
+console.log(v3)
